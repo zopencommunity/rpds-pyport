fadb15f6ee149481d895f2c66c09f54b89d44504
Igor Todorovski <itodorov@ca.ibm.com>
2026-08-10 17:04:18 -0400

    Rename: drop python- prefix from repo name and internal references

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/python-rpds-pyport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/rpds-pyport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/python-rpds-pyport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/rpds-pyport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'Python bindings to Rust's persistent data structures (rpds)'),
       string(name: 'BUILD_LINE', value: 'DEV'),
       booleanParam(name: 'PUBLISH_PYTHON_WHEEL', value: true)
